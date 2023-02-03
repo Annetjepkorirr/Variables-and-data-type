f2057858f2f69d4be7b62d55f50e2811b320b1b5
Annetjepkorirr <annetjepkorir>
2023-02-03 08:19:41 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    var fullname = "Banice alima"
+    println(fullname)
+
+    var age: (Byte) = 20
+    println(age)
+
+    var phoneNumber: (Int) = 5489025
+    println(phoneNumber)
+
+    var weightinKg: (Short) = 55
+    println(weightinKg)
+
+    var kenyacitizen: (Boolean) = true
+    println(kenyacitizen)
+}
